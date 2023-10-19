d4220c9fc9ada334c659242dcc185ea97dd57499
Mukhammadjon <93770723+Muhammadjon-is@users.noreply.github.com>
2023-10-19 17:56:40 +0200

    REST API DONE!

--- routes/empoleys.js
+++ routes/empoleys.js
@@ -1,93 +1,137 @@
-import express from "express"
+import express from "express";
 import sequelize from "../config/db.js";
 
 const employeRouter = express.Router();
 
-employeRouter.get('/', async (req, res) => {
-    try {
-         const employers = await sequelize.query(
-            `SELECT * FROM employer`
-         );
-         res.status(200).json(employers[0]);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "An error occurred while fetching employers." });
+employeRouter.get("/", async (req, res) => {
+  try {
+    const employers = await sequelize.query(`SELECT * FROM employer`);
+    res.status(200).json(employers[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching employers." });
+  }
+});
+
+// Get EMPLOYER BY ID
+employeRouter.get("/:id", async (req, res) => {
+  try {
+    const employerId = req.params.id;
+
+    const employer = await sequelize.query(
+      `
+            SELECT * 
+            FROM employer
+            LEFT JOIN job ON job.id = employer.job_id
+            WHERE employer.id = :id
+        `,
+      {
+        replacements: { id: employerId },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
+
+    if (employer.length === 0) {
+      return res.status(404).json({ error: "Employer not found." });
     }
+
+    res.status(200).json(employer[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({
+        error: "An error occurred while fetching an employer.",
+        details: error.message,
+      });
+  }
 });
 
 // Add new Employer
-employeRouter.post('/add', async (req, res) => {
-    try {
-        const { name, salary, degree, job_id } = req.body;
-        const newEmployer = await sequelize.query(
-            `INSERT INTO employer (name, degree, salary, job_id) VALUES (:name, :degree, :salary, :job_id) RETURNING *`,
-            {
-                replacements: { name, degree, salary, job_id },
-                type: sequelize.QueryTypes.INSERT
-            }
-        );
-        res.status(201).json(newEmployer[0]);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "An error occurred while adding an employer." });
-    }
+employeRouter.post("/add", async (req, res) => {
+  try {
+    const { name, salary, degree, job_id } = req.body;
+    const newEmployer = await sequelize.query(
+      `INSERT INTO employer (name, degree, salary, job_id) VALUES (:name, :degree, :salary, :job_id) RETURNING *`,
+      {
+        replacements: { name, degree, salary, job_id },
+        type: sequelize.QueryTypes.INSERT,
+      }
+    );
+    res.status(201).json(newEmployer[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while adding an employer." });
+  }
 });
 
 // Edit/Update Employer
-employeRouter.put('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name, salary, degree, job_id } = req.body;
-        
-        const oldEmployer = await sequelize.query('SELECT * FROM employer WHERE id = :id', {
-            replacements: { id },
-            type: sequelize.QueryTypes.SELECT,
-        });
-        
-        if (oldEmployer.length === 0) {
-            return res.status(404).json({ error: 'Employer not found.' });
-        }
+employeRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, salary, degree, job_id } = req.body;
 
-        const updatedEmployer = await sequelize.query(
-            'UPDATE employer SET name = :name, degree = :degree, salary = :salary, job_id = :job_id WHERE id = :id RETURNING *',
-            {
-                replacements: {
-                    name: name || oldEmployer[0].name,
-                    degree: degree || oldEmployer[0].degree,
-                    salary: salary || oldEmployer[0].salary,
-                    job_id: job_id || oldEmployer[0].job_id,
-                    id,
-                },
-                type: sequelize.QueryTypes.UPDATE,
-            }
-        );
+    const oldEmployer = await sequelize.query(
+      "SELECT * FROM employer WHERE id = :id",
+      {
+        replacements: { id },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
 
-        res.status(200).json(updatedEmployer[0]);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "An error occurred while updating an employer.", details: error.message });
+    if (oldEmployer.length === 0) {
+      return res.status(404).json({ error: "Employer not found." });
     }
+
+    const updatedEmployer = await sequelize.query(
+      "UPDATE employer SET name = :name, degree = :degree, salary = :salary, job_id = :job_id WHERE id = :id RETURNING *",
+      {
+        replacements: {
+          name: name || oldEmployer[0].name,
+          degree: degree || oldEmployer[0].degree,
+          salary: salary || oldEmployer[0].salary,
+          job_id: job_id || oldEmployer[0].job_id,
+          id,
+        },
+        type: sequelize.QueryTypes.UPDATE,
+      }
+    );
+
+    res.status(200).json(updatedEmployer[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({
+        error: "An error occurred while updating an employer.",
+        details: error.message,
+      });
+  }
 });
 
 // Delete Empoyer
 
-employeRouter.delete('/:id', async (req, res) => {
-    try {
-        await sequelize.query('DELETE FROM employer WHERE id = :id', {
-            replacements: { id: req.params.id },
-            type: sequelize.QueryTypes.DELETE,
-        });
-        
-        res.status(204).end(); // Return a 204 No Content status upon successful deletion
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "An error occurred while deleting an employer.", details: error.message });
-    }
+employeRouter.delete("/:id", async (req, res) => {
+  try {
+    await sequelize.query("DELETE FROM employer WHERE id = :id", {
+      replacements: { id: req.params.id },
+      type: sequelize.QueryTypes.DELETE,
+    });
+
+    res.status(204).end(); // Return a 204 No Content status upon successful deletion
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({
+        error: "An error occurred while deleting an employer.",
+        details: error.message,
+      });
+  }
 });
-
-
-
-
-
 
 export default employeRouter;
--- routes/jobs.js
+++ routes/jobs.js
@@ -31,7 +31,33 @@ jobsRouter.post("/add", async (req, res) => {
     res.status(500).json({ error: "An error occurred while adding a job." });
   }
 });
+  
+jobsRouter.delete('/:id', async (req, res) => {
+  try {
+    const jobId = req.params.id;
 
+    // Delete employers associated with the job first
+    await sequelize.query('DELETE FROM employer WHERE job_id = :jobId', {
+      replacements: { jobId },
+      type: sequelize.QueryTypes.DELETE,
+    });
+
+    // Delete the job
+    await sequelize.query('DELETE FROM job WHERE id = :jobId', {
+      replacements: { jobId },
+      type: sequelize.QueryTypes.DELETE,
+    });
+
+    res.status(200).json({ message: "Job Deleted" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while deleting a job.", details: error.message });
+  }
+});
+
+
+
+// Delete job
 
 
 export default jobsRouter;
